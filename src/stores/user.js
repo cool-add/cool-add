0db0121655e90de962d78a75aefea6fd588dea4d
可乐鸡翅呀 <3054127379@qq.com>
2024-09-09 10:14:57 +0800

    配置路由

---
+++ src/stores/user.js
@@ -0,0 +1,26 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+// 用户模块 token setToken removeToken
+export const useUserStore = defineStore(
+  'big-user',
+  () => {
+    const token = ref('')
+    const setToken = (newToken) => {
+      token.value = newToken
+    }
+    const removeToken = () => {
+      token.value = ''
+    }
+
+    return {
+      token,
+      setToken,
+      removeToken
+    }
+  },
+  {
+    // 持久化配置
+    persist: true
+  }
+)
